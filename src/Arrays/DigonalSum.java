f3c782e8224c5d0626dff4ebcf791467863de395
Prathmesh07778 <202201961@vupune.ac.in>
2025-03-24 23:38:07 +0530

    Fourth Day Code

---
+++ src/Arrays/DigonalSum.java
@@ -0,0 +1,32 @@
+public class DigonalSum{
+    public static void DigonalSum(int[][] arr) {
+        if (arr.length != arr[0].length) {
+            throw new IllegalArgumentException("Matrix must be square");
+        }
+
+        int mainDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            // Main diagonal (top-left to bottom-right)
+            mainDiagonalSum += arr[i][i];
+
+            // Secondary diagonal (top-right to bottom-left)
+            secondaryDiagonalSum += arr[i][arr.length - 1 - i];
+        }
+
+        System.out.println("Main Diagonal Sum: " + mainDiagonalSum);
+        System.out.println("Secondary Diagonal Sum: " + secondaryDiagonalSum);
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        DiagonalSum(matrix);
+    }
+}
---
+++ src/Arrays/TwoDiamentional.java
@@ -0,0 +1,10 @@
+package Arrays;
+import java.util.Scanner;
+
+public class TwoDiamentional {
+
+    public static void main(String args[]){
+
+        int [][] arr = {};
+    }
+}
